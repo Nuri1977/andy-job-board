e807104eae6c0fd4077c6aef020fde5fef45db4d
nuri1977 <nurilacka@gmail.com>
2020-05-11 23:02:32 +0200

    carrierwave uploader cretated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200511204623) do
+ActiveRecord::Schema.define(version: 20200511205438) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20200511204623) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
